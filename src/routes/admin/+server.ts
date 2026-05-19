3a2c8a3e3130b76e9a113d85cdcad9b8b4722419
Robert Z <r.zioltkowski@gmail.com>
2026-05-19 11:34:27 +0200

    switch back to Decap CMS, prepare config for DecapBridge

--- src/routes/admin/+server.ts
+++ src/routes/admin/+server.ts
@@ -5,10 +5,9 @@ const HTML = `<!DOCTYPE html>
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta name="robots" content="noindex" />
   <title>Admin - ASV Rotauge</title>
-  <link rel="cms-config-url" href="/config.yml" />
 </head>
 <body>
-  <script src="https://unpkg.com/@sveltia/cms/dist/sveltia-cms.js"></script>
+  <script src="https://unpkg.com/decap-cms@^3.0.0/dist/decap-cms.js"></script>
 </body>
 </html>`;
 
